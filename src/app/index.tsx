17ae5a656b03d499fcb81405c2244bc36843b948
Joshua Wijaya <joshua.wijaya@edu.sait.ca>
2026-06-06 23:58:42 -0600

    Final

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -20,30 +20,8 @@ export default function HomeScreen() {
         </View>
         <View style={{ width: 24 }} />
       </View>
-      {/* TOP BAR */}
-      <View>
-        <Text>ootd_everyday</Text>
-        <Text>Via: frenchie_fry39</Text>
-      </View>
-      ;{/* Likes */}
-      <View>
-        <Text>Liked by paisley.print.48 and 7 others</Text>
-      </View>
-      ;{/* Caption */}
-      <View>
-        <Text>Fresh shot on a sunny day!🌞 </Text>
-      </View>
-      ;{/* Comments */}
-      <View>
-        <Text>View all 12 comments</Text>
-        <Text>lil_wyatt838: Awesome tones</Text>
-        <Text>pia.in.a.pod: Gorg. Love it! ❤️</Text>
-      </View>
-      ;{/* Time */}
-      <View>
-        <Text>1 day ago</Text>
-      </View>
-      ;{/* Post Header */}
+
+      {/* Post Header */}
       <View style={styles.postHeader}>
         <Image
           source={{
